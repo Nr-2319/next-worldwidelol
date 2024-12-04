8ffe0457b9764f5701e7f97401cd29dcd3e6cdfc
Nishant Raj <nishantbb7@gmail.com>
2024-12-04 11:44:29 +0530

    created home page and hero section

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,20 @@
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <section className="w-full flex-col flex-center">
+            <h1 className="head_text text-center">
+                Connect. Share.
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">
+                    LOL
+                </span>
+            </h1>
+            <p className="desc text-center">
+            WorldWideLOL is an open-source platform for users to discover, create, and share jokes, memes, and funny moments in one vibrant community. WorldWideLOL is where the world unites to LOL!
+            </p>
+
+            {/* Feed */}
+        </section>
+    );
 };
 
 export default Home;
